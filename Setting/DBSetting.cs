bfdee048df788ccc4a368705932ea91bba597b4c
SKY-HaYaTo <79740765+SKY-HaYaTo@users.noreply.github.com>
2021-03-28 22:41:59 +0900

    Create DBSetting.cs

---
+++ Setting/DBSetting.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesManagementSystem.Setting
+{
+    public class DBSetting
+    {
+        public string ConnectionString { get; set; }
+    }
+}
